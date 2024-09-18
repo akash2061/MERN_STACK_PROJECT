0ddd4c1d86bd22b16b8ccd6a4568f0a1176b58be
akash2061 <lucifermorningstar2061@gmail.com>
2024-09-18 20:46:25 +0530

    entry file

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const authRoutes = require("./router/user")
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", authRoutes);
+connectDB();
+
+app.listen(5000, () => {
+    console.log("Server is running on http://localhost:5000");
+});
