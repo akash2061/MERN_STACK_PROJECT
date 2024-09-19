a415b8f47ec32ea2106ae9032dd189000aceb5b4
akash2061 <lucifermorningstar2061@gmail.com>
2024-09-19 15:15:21 +0530

    added error handling

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./router/user");
+const errorHandler = require("./middleware/globalErrorHandler");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/auth", authRoutes);
 connectDB();
+
+app.use("/auth", authRoutes);
+app.use(errorHandler);
 
 app.listen(5000, () => {
     console.log("Server is running on http://localhost:5000");
