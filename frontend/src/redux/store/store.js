a9d1e280d88191636919e050e5de65efed6953c9
akash2061 <lucifermorningstar2061@gmail.com>
2024-10-06 20:27:42 +0530

    Store update

--- frontend/src/redux/store/store.js
+++ frontend/src/redux/store/store.js
@@ -1,9 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../slices/authSlice"
+import authSlice from "../slices/authSlice";
+import productSlice from "../slices/productSlice";
+import cartSlice from "../slices/cartSlice";
+import userSlice from "../slices/userSlice";
 
 const store = configureStore({
-    reducer: {
-        auth: authReducer,
+    reducer : {
+        auth : authSlice,
+        product : productSlice,
+        cart : cartSlice,
+        user : userSlice
     }
 });
 
