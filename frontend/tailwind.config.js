90e350aa2d30b4dfeaf20c4a94dbf4f351e3de9d
akash2061 <lucifermorningstar2061@gmail.com>
2024-10-13 23:14:08 +0530

    Updates

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,11 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['SamsungOne', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
