176e6b14eb0990eb084625c2c58180182e576bab
akash2061 <lucifermorningstar2061@gmail.com>
2024-10-14 21:20:51 +0530

    Update footer styling

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-gray-300 py-10">
+    <footer className="bg-zinc-900 border-t-4 border-stone-800 text-gray-300 py-10">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-4 md:gap-8">
           {/* Product Categories */}
